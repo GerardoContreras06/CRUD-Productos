7cd5a088bae3015cdda203c5e4ef4eb1dc7cecb7
GerardoContreras06 <gerardocontrerasestrada@gmail.com>
2024-07-19 10:05:24 -0600

    Agregar proyecto a Repositorio

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import org.gerardocontreras.webapp.model.Producto;
+import org.gerardocontreras.webapp.util.JpaUtil;
+
+public class Main {
+    
+    private static EntityManager em = JpaUtil.getEntityManager();
+    
+    public static void main(String [] args){
+            
+        TypedQuery<Producto> query = em.createQuery("SELECT p FROM Producto p", Producto.class);
+        List <Producto> productos = query.getResultList();
+        
+        productos.forEach(producto -> System.out.println(producto));
+        
+    }
+}
